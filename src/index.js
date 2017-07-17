882410ab8a8c050b4e1b083e8f24e138bd14e1c3
Roman Pushkov <pushkovroman@me.com>
2017-07-18 01:10:53 +0300

    fixing errors

--- src/index.js
+++ src/index.js
@@ -3,4 +3,4 @@ import readlineSync from 'readline-sync';
 export default () => {
   const playerName = readlineSync.question('\nMay I know your name? ');
   console.log(`Hello, ${playerName}!`);
-}
+};
