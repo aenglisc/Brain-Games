3dbcb96b9891580dae5f07edabf29f45f580e1eb
Roman Pushkov <pushkovroman@me.com>
2017-07-17 21:40:07 +0300

    index.js logic bin call

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import readlineSync from 'readline-sync';
+
+export default () => {
+  const playerName = readlineSync.question('\nMay I know your name? ');
+  console.log(`Hello, ${playerName}!`);
+}
