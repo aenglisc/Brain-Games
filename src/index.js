3b63e988e3628886e4a737a47f2db165b2c72500
Roman Pushkov <pushkovroman@me.com>
2017-07-25 01:12:06 +0300

    added comments

--- src/index.js
+++ src/index.js
@@ -2,25 +2,31 @@
 import readlineSync from 'readline-sync';
 
 export default (game, description) => {
+  // greet the player, describe the game when possible
   console.log('\nWelcome to the Brain Games!');
   if (description) { console.log(description); }
 
+  // ask for a name
   const playerName = readlineSync.question('\nMay I know your name? ');
   console.log(`Hello, ${playerName}!`);
   const answersToWin = 3;
 
   const playRound = (round) => {
     if (round - 1 === answersToWin) {
+      // victory
       console.log(`\nCongratulations, ${playerName}!\n`);
     } else {
+      // play round
       const { expectedAnswer, questionString } = game();
       const question = `\nRound ${round}: ${questionString}`;
       const answer = readlineSync.question(question);
 
       if (answer === expectedAnswer) {
+        // call next round
         console.log('Correct!');
         playRound(round + 1);
       } else {
+        // defeat
         console.log(`Sorry, "${answer}" is the wrong answer. :(`);
         console.log(`The correct answer is "${expectedAnswer}".`);
         console.log(`\nLet's try again, ${playerName}!\n`);
@@ -28,5 +34,6 @@ export default (game, description) => {
     }
   };
 
+  // start the game
   if (game) { playRound(1); }
 };
--- src/utils.js
+++ src/utils.js
@@ -1,12 +1,15 @@
 
+// generates a random integer between min and max (inclusive), 1 to 50 by default
 export const getRandomInt = (min = 1, max = 50) =>
   Math.floor(Math.random() * (1 + (Math.ceil(max) - Math.ceil(min)))) + Math.ceil(min);
 
+// returns the greatest common divisor of two numbers
 export const gcd = (x = 0, y = 0) => {
   if (y === 0) { return x; }
   return gcd(y, x % y);
 };
 
+// returns a sorted array of digits from a number
 export const numberToSortedArrayOfDigits = (num) => {
   const stringFromNum = num.toString();
   const sortedArrayFromString = stringFromNum.split('').sort();
@@ -14,6 +17,7 @@ export const numberToSortedArrayOfDigits = (num) => {
   return arrayOfDigitsFromArray;
 };
 
+// returns a balanced number (every next digit is equal to or greater by 1 than the previous)
 export const balance = (num) => {
   const digitsArray = numberToSortedArrayOfDigits(num);
   const recBalance = (array) => {
@@ -33,11 +37,11 @@ export const balance = (num) => {
   return recBalance(digitsArray);
 };
 
+// generates a string representing an arithmetic progression
 export const genArithProg = () => {
   const base = getRandomInt(1, 100);
   const increment = getRandomInt(1, 10);
   const length = getRandomInt(5, 10);
-
   const iterGen = (str, element, acc) => {
     if (acc === 0) { return str; }
     return iterGen(acc === 1 ? `${str}${element}`
@@ -47,6 +51,7 @@ export const genArithProg = () => {
   return iterGen('', base, length);
 };
 
+// checks for primality
 export const isPrime = (num) => {
   const sqrtNum = Math.sqrt(num);
   if (num <= 1) { return false; }
